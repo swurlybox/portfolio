f9c460a8d91f0187feaa4fbc9a992cae70ccebba
Edwin Wu <ewu8@sfsu.edu>
2025-07-18 14:26:10 -0700

    update and delete posts api

--- backend/src/controllers/posts.ts
+++ backend/src/controllers/posts.ts
@@ -65,4 +65,76 @@ export const createPost: RequestHandler<unknown, unknown, CreatePostBody, unknow
     }
 }
 
+interface UpdatePostParams {
+    postId: string
+}
 
+interface UpdatePostBody {
+    title?: string,
+    description?: string,
+    imageFilePath?: string
+}
+
+export const updatePost: RequestHandler<UpdatePostParams, unknown, UpdatePostBody, unknown> = async (req, res, next) => {
+
+    const {title : newTitle, description : newDescription} = req.body;
+    const postId = req.params.postId;
+
+    // Check the postId, if its valid, then make the update
+    // ODO: Only the original poster of the post should be allowed to make the update
+    // If its invalid, dont do any updates
+
+    try {
+        
+        if(!mongoose.isValidObjectId(postId)){
+            throw createHttpError(400, "Invalid post id");
+        }
+
+        if (!newTitle || !newDescription){
+            throw createHttpError(400, "Updated post must have a title and description");
+        }
+
+        const post = await PostModel.findById(postId).exec();
+
+        if (!post) {
+            throw createHttpError(404, "Post not found")
+        }
+
+        post.title = newTitle;
+        post.description = newDescription;
+
+        const updatedPost = await post.save();
+
+        res.status(200).json(updatedPost);
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const deletePost: RequestHandler = async (req, res, next) => {
+
+    const postId = req.params.postId;
+
+    // TODO: Verify that only the original poster can delete this post.
+
+    try {
+
+        if(!mongoose.isValidObjectId(postId)){
+            throw createHttpError(400, "Invalid post id");
+        }
+
+        const post = await PostModel.findById(postId).exec();
+
+        if (!post){
+            throw createHttpError(404, "Post not found");
+        }
+
+        await post.deleteOne();
+
+        res.sendStatus(204);
+        
+    } catch (error) {
+        next(error)
+    }
+}
--- backend/src/routes/posts.ts
+++ backend/src/routes/posts.ts
@@ -4,9 +4,10 @@ import * as PostsController from "../controllers/posts";
 const router = express.Router();
 
 router.get("/", PostsController.getPosts);
+router.post("/", PostsController.createPost);
 
 router.get("/:postId", PostsController.getPost);
-
-router.post("/", PostsController.createPost);
+router.patch("/:postId", PostsController.updatePost);
+router.delete("/:postId", PostsController.deletePost);
 
 export default router;

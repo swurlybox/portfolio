adfd037196e3a413fa990c45bd84d1eb7bfb1ea8
Edwin Wu <ewu8@sfsu.edu>
2025-07-18 20:37:16 -0700

    create-react-app and tinkered with creating a react component with bootstrap - learned useState(); concept of state variables and functions that update state - react updates UI automatically upon changes to state variables

---
+++ frontend/src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+
+  const [clickCount, setClickCount] = useState(0);
+
+
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Making an edit to the react app.
+        </p>
+        
+        <Button onClick={() => setClickCount(clickCount + 1)}>
+          Click {clickCount} times
+        </Button>
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
